967dc41a70a9e17789e87c944e5b426b31822ed9
ElAlgoritmoDeLaNoche <elalgoritmodelanochebazinga@gmail.com>
2022-10-27 13:43:14 -0500

    Agregar archivo vercel

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.CONNECTION_URL)
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('DB online')
   } catch (error) {
     console.log(error)
