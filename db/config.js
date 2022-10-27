18952a87b25e55fbcec3f122dd3406642373bfd3
ElAlgoritmoDeLaNoche <elalgoritmodelanochebazinga@gmail.com>
2022-10-27 13:47:33 -0500

    Cambio database

--- db/config.js
+++ db/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.CONNECTION_URL)
     console.log('DB online')
   } catch (error) {
     console.log(error)
